b51f885a6143d489fd306ac381ebfc1325181b6f
Lizari <muroya725@gmail.com>
2022-07-21 21:42:20 +0900

    ファイル形式からjsonに変えたの修正

--- src/utils/APIService.ts
+++ src/utils/APIService.ts
@@ -2,6 +2,7 @@ import {Config} from "@/libs/Config";
 import axios from "axios";
 
 type PostArticleData = {
+    id: number,
     title: string,
     description: string,
     thumbnail: string,
@@ -11,32 +12,32 @@ type PostArticleData = {
 
 const client = axios.create({
     baseURL: Config.API_URL,
-    withCredentials: true,
+    withCredentials: false,
 });
 
 export const get = () => {
-    return client.get("/article");
+    return client.get("/article", {
+        responseEncoding: 'utf8',
+        responseType: 'json'
+    });
 }
 
 export const postArticle = (id: string, password: string, article: PostArticleData): Promise<boolean | undefined> => {
-    const formData = new FormData();
-    const data = [
-        "===",
-        `title: ${article.title}`,
-        `description: ${article.description}`,
-        `thumbnail: ${article.thumbnail}`,
-        `tags: ${article.tags}`,
-        "===",
-        ...article.content.split(/\r?\n/)].join("\n");
-    formData.append("file", stringToFile(article.title, [data]));
+    const data = {
+        'title': article.title,
+        'description': article.description,
+        'thumbnail': article.thumbnail,
+        'tags': article.tags,
+        'content': article.content.split(/\r?\n/).join('\n')
+    }
 
-    return client.post("/blog/upload", formData, {
+    return client.patch(`/article/${article.id}`, data, {
         auth: {
           username: id,
           password: password
         },
         headers: {
-            "Content-Type": "multipart/from-data",
+            "Content-Type": "application/json",
         },
     }).then((res) => {
         return res.status === 200;
